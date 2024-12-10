6d090857c4be242a47db14e8a6c414e0f5be9ee6
Tayeb Sedraia <tayeb.sedraia@orange.com>
2024-12-10 15:23:57 +0100

    refactor: move demo app into 'app' folder

--- app/test/widget_test.dart
+++ app/test/widget_test.dart
@@ -11,7 +11,7 @@ import 'package:ouds_flutter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our lib and trigger a frame.
+    // Build our app and trigger a frame.
     await tester.pumpWidget(const OudsApplication());
 
     // Verify that our counter starts at 0.
