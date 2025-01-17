f11fb55d4e63e8b3496a7bdca6fdccd9cff6053b
Tayeb Sedraia <tayeb.sedraia@orange.com>
2025-01-17 17:21:40 +0100

    docs: add license header to files projects

--- library/lib/theme_contract/theme/ouds_border_modifier.dart
+++ library/lib/theme_contract/theme/ouds_border_modifier.dart
@@ -1,6 +1,17 @@
-import 'package:flutter/material.dart';
+/*
+ * Software Name : OUDS Flutter
+ * SPDX-FileCopyrightText: Copyright (c) Orange SA
+ * SPDX-License-Identifier: MIT
+ *
+ * This software is distributed under the MIT license,
+ * the text of which is available at https://opensource.org/license/MIT/
+ * or see the "LICENSE" file for more details.
+ *
+ * Software description: Flutter library of reusable graphical components for Android and iOS
+ */
 
-import 'ouds_border_style.dart';
+import 'package:flutter/material.dart';
+import 'package:ouds_flutter/theme_contract/theme/ouds_border_style.dart';
 
 class OudsBorderModifier {
   final OudsBorderStyle borderStyle;
--- library/lib/theme_contract/theme/tokens/ouds_space_key_token.dart
+++ library/lib/theme_contract/theme/tokens/ouds_space_key_token.dart
@@ -1,3 +1,15 @@
+/*
+ * Software Name : OUDS Flutter
+ * SPDX-FileCopyrightText: Copyright (c) Orange SA
+ * SPDX-License-Identifier: MIT
+ *
+ * This software is distributed under the MIT license,
+ * the text of which is available at https://opensource.org/license/MIT/
+ * or see the "LICENSE" file for more details.
+ *
+ * Software description: Flutter library of reusable graphical components for Android and iOS
+ */
+
 import 'package:ouds_flutter/theme_contract/theme/tokens/ouds_key_tokens.dart';
 
 class OudsSpaceKeyToken extends OudsKeyToken {
