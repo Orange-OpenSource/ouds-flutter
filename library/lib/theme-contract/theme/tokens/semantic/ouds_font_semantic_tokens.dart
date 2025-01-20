d4894b1cc90a4193cfce7b3aab964a8347bc5552
Tayeb Sedraia <tayeb.sedraia@orange.com>
2025-01-20 15:41:56 +0100

    chore: add semantic tokens to theme-contract

---
+++ library/lib/theme-contract/theme/tokens/semantic/ouds_font_semantic_tokens.dart
@@ -0,0 +1,25 @@
+/*
+ * Software Name : OUDS Flutter
+ * SPDX-FileCopyrightText: Copyright (c) Orange SA
+ * SPDX-License-Identifier: MIT
+ *
+ * This software is distributed under the MIT license,
+ * the text of which is available at https://opensource.org/license/MIT/
+ * or see the "LICENSE" file for more details.
+ *
+ * Software description: Flutter library of reusable graphical components for Android and iOS
+ */
+
+import 'package:flutter/material.dart';
+
+class OudsFontSemanticTokens {
+  final TextStyle headline;
+  final TextStyle body;
+  final TextStyle caption;
+
+  OudsFontSemanticTokens({
+    required this.headline,
+    required this.body,
+    required this.caption,
+  });
+}
