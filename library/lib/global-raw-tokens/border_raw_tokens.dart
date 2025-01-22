cb28a92cabf82a875805c73bf437b24ada8f2125
Tayeb Sedraia <tayeb.sedraia@orange.com>
2025-01-22 09:52:29 +0100

    chore: add border raw tokens to global raw tokens

---
+++ library/lib/global-raw-tokens/border_raw_tokens.dart
@@ -0,0 +1,47 @@
+/*
+ * Software Name : OUDS Flutter
+ * SPDX-FileCopyrightText: Copyright (c) Orange SA
+ * SPDX-License-Identifier: MIT
+ *
+ * This software is distributed under the MIT license,
+ * the text of which is available at https://opensource.org/license/MIT/
+ * or see the "LICENSE" file for more details.
+ *
+ * Software description: Flutter library of reusable graphical components for Android and iOS
+ */
+
+class BorderRawTokens {
+  // Base pour les calculs de bordures
+  static const int borderBase = 4;
+
+  // Rayons de bordure (en pixels)
+  static const double borderRadius9999 = 2000.0;
+  static const double borderRadius0 = borderBase * 0.0; // 0
+  static const double borderRadius25 = borderBase * 0.25; // 1
+  static const double borderRadius50 = borderBase * 0.5; // 2
+  static const double borderRadius75 = borderBase * 0.75; // 3
+  static const double borderRadius100 = borderBase * 1.0; // 4
+  static const double borderRadius150 = borderBase * 1.5; // 6
+  static const double borderRadius200 = borderBase * 2.0; // 8
+  static const double borderRadius300 = borderBase * 3.0; // 12
+  static const double borderRadius400 = borderBase * 4.0; // 16
+  static const double borderRadius500 = borderBase * 5.0; // 20
+  static const double borderRadius600 = borderBase * 6.0; // 24
+  static const double borderRadius800 = borderBase * 8.0; // 32
+
+  // Largeurs de bordure (en pixels)
+  static const double borderWidth0 = borderBase * 0.0; // 0
+  static const double borderWidth25 = borderBase * 0.25; // 1
+  static const double borderWidth50 = borderBase * 0.5; // 2
+  static const double borderWidth75 = borderBase * 0.75; // 3
+  static const double borderWidth100 = borderBase * 1.0; // 4
+  static const double borderWidth125 = borderBase * 1.25; // 5
+  static const double borderWidth150 = borderBase * 1.5; // 6
+  static const double borderWidth200 = borderBase * 2.0; // 8
+
+  // Styles de bordure
+  static const String borderStyleDashed = 'dashed';
+  static const String borderStyleDotted = 'dotted';
+  static const String borderStyleNone = 'none';
+  static const String borderStyleSolid = 'solid';
+}
