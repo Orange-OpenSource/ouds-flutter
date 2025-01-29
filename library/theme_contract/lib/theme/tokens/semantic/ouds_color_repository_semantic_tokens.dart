f0ec464bf6a59153a04f125fe4804528a3b0ea80
Tayeb Sedraia <tayeb.sedraia@orange.com>
2025-01-29 11:00:29 +0100

    chore: update semantic for repository token

--- library/theme_contract/lib/theme/tokens/semantic/ouds_color_repository_semantic_tokens.dart
+++ library/theme_contract/lib/theme/tokens/semantic/ouds_color_repository_semantic_tokens.dart
@@ -11,11 +11,16 @@
  */
 
 import 'package:flutter/material.dart';
+import 'package:global_raw_tokens/color_raw_tokens.dart';
+import 'package:global_raw_tokens/orange_brand_color_raw_tokens.dart';
 
 class OudsColorRepositorySemanticTokens {
-  static const Color repositoryAccentDefaultLight = Color(0xFFF4A300);
-  static const Color repositoryAccentHighestLight = Color(0xFF212121);
-  static const Color repositoryAccentLowLight = Color(0xFFF8D300);
+  static const Color repositoryAccentDefaultLight =
+      ColorRawTokens.colorFunctionalSun500;
+  static const Color repositoryAccentHighestLight =
+      OrangeBrandColorRawTokens.colorWarmGray900;
+  static const Color repositoryAccentLowLight =
+      ColorRawTokens.colorFunctionalSun300;
   static const Color repositoryAccentLowestLight = Color(0xFFF1F1F1);
   static const Color repositoryAccentDefaultDark = Color(0xFFF4A300);
 
