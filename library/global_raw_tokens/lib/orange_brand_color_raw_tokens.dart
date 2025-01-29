b5569d89e5ed4b20d9a08dc3970831b296b08160
Tayeb Sedraia <tayeb.sedraia@orange.com>
2025-01-29 10:57:19 +0100

    chore: update global raw tokens for orange color

--- library/global_raw_tokens/lib/orange_brand_color_raw_tokens.dart
+++ library/global_raw_tokens/lib/orange_brand_color_raw_tokens.dart
@@ -16,8 +16,21 @@ class OrangeBrandColorRawTokens {
   static const Color colorOrange50 = Color(0xfffff2e6);
   static const Color colorOrange100 = Color(0xffffd5b0);
   static const Color colorOrange200 = Color(0xffffc18a);
+  static const Color colorOrange300 = Color(0xffffa554);
+  static const Color colorOrange400 = Color(0xffff9433);
+  static const Color colorOrange500 = Color(0xffff7900);
   static const Color colorOrange550 = Color(0xfff15e00);
-
+  static const Color colorOrange600 = Color(0xffe86e00);
+  static const Color colorOrange700 = Color(0xffb55600);
+  static const Color colorOrange800 = Color(0xff8c4300);
+  static const Color colorOrange900 = Color(0xff6b3300);
   static const Color colorWarmGray100 = Color(0xfff9f5f0);
+  static const Color colorWarmGray200 = Color(0xffe9ddce);
+  static const Color colorWarmGray300 = Color(0xffd6c4ae);
+  static const Color colorWarmGray400 = Color(0xffc1ab90);
+  static const Color colorWarmGray500 = Color(0xffa99275);
+  static const Color colorWarmGray600 = Color(0xff8a7860);
+  static const Color colorWarmGray700 = Color(0xff685d50);
+  static const Color colorWarmGray800 = Color(0xff48433d);
   static const Color colorWarmGray900 = Color(0xff353228);
 }
