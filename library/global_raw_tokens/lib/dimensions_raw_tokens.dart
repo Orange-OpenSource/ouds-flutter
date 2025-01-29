838c4c29a60bb83b464e5755d23366a9e3c30821
Tayeb Sedraia <tayeb.sedraia@orange.com>
2025-01-29 10:56:28 +0100

    chore: update global raw tokens for dimensions

--- library/global_raw_tokens/lib/dimensions_raw_tokens.dart
+++ library/global_raw_tokens/lib/dimensions_raw_tokens.dart
@@ -11,9 +11,40 @@
  */
 
 class DimensionRawTokens {
-  static const double buttonHeight = 48.0;
-  static const double buttonRadius = 8.0;
-  static const double inputFieldHeight = 8.0;
-  static const double gridSize = 8.0;
-  static const double gutterSize = 8.0;
+  static const double dimensionBase = 4.0;
+  static const double dimension0 = dimensionBase * 0.0; // 0
+  static const double dimension25 = dimensionBase * 0.5; // 2
+  static const double dimension50 = dimensionBase * 1.0; // 4
+  static const double dimension75 = dimensionBase * 1.5; // 6
+  static const double dimension100 = dimensionBase * 2.0; // 8
+  static const double dimension125 = dimensionBase * 2.5; // 10
+  static const double dimension150 = dimensionBase * 3.0; // 12
+  static const double dimension200 = dimensionBase * 4.0; // 16
+  static const double dimension250 = dimensionBase * 5.0; // 20
+  static const double dimension300 = dimensionBase * 6.0; // 24
+  static const double dimension350 = dimensionBase * 7.0; // 28
+  static const double dimension400 = dimensionBase * 8.0; // 32
+  static const double dimension450 = dimensionBase * 9.0; // 36
+  static const double dimension500 = dimensionBase * 10.0; // 40
+  static const double dimension550 = dimensionBase * 11.0; // 44
+  static const double dimension600 = dimensionBase * 12.0; // 48
+  static const double dimension650 = dimensionBase * 13.0; // 52
+  static const double dimension700 = dimensionBase * 14.0; // 56
+  static const double dimension750 = dimensionBase * 15.0; // 60
+  static const double dimension800 = dimensionBase * 16.0; // 64
+  static const double dimension850 = dimensionBase * 17.0; // 68
+  static const double dimension900 = dimensionBase * 18.0; // 72
+  static const double dimension1000 = dimensionBase * 20.0; // 80
+  static const double dimension1200 = dimensionBase * 24.0; // 96
+  static const double dimension1400 = dimensionBase * 28.0; // 112
+  static const double dimension1600 = dimensionBase * 32.0; // 128
+  static const double dimension1800 = dimensionBase * 36.0; // 144
+  static const double dimension2000 = dimensionBase * 40.0; // 160
+  static const double dimension3000 = dimensionBase * 80.0; // 320
+  static const double dimension4000 = dimensionBase * 120.0; // 480
+  static const double dimension5000 = dimensionBase * 140.0; // 560
+  static const double dimension6000 = dimensionBase * 160.0; // 640
+  static const double dimension7000 = dimensionBase * 180.0; // 720
+  static const double dimension9000 = dimensionBase * 220.0; // 880
+  static const double dimension11000 = dimensionBase * 260.0; // 1040
 }
