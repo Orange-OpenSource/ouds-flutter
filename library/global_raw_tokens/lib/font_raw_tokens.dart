671f9915f071218b709c3f9564714e405e2d8de6
Tayeb Sedraia <tayeb.sedraia@orange.com>
2025-01-29 10:56:47 +0100

    chore: update global raw tokens for font

---
+++ library/global_raw_tokens/lib/font_raw_tokens.dart
@@ -0,0 +1,76 @@
+/*
+ * Software Name : OUDS Flutter
+ * SPDX-FileCopyrightText: Copyright (c) Orange SA
+ * SPDX-License-Identifier: MIT
+ *
+ * This software is distributed under the MIT license,
+ * the text of which is available at https://opensource.org/license/MIT/
+ * or see the "LICENSE" file for more details.
+ *
+ * Software description: Flutter library of reusable graphical components for Android and iOS
+ */
+
+class FontRawTokens {
+  static const double fontLetterSpacing150 = 0.18;
+  static const double fontLetterSpacing175 = 0.19;
+  static const double fontLetterSpacing200 = 0.17;
+  static const double fontLetterSpacing250 = 0.2;
+  static const double fontLetterSpacing300 = 0.18;
+  static const double fontLetterSpacing350 = 0.0;
+  static const double fontLetterSpacing450 = -0.12;
+  static const double fontLetterSpacing550 = -0.28;
+  static const double fontLetterSpacing650 = -0.32;
+  static const double fontLetterSpacing750 = -0.36;
+  static const double fontLetterSpacing850 = -0.4;
+  static const double fontLetterSpacing950 = -0.55;
+  static const double fontLetterSpacing1050 = -0.6;
+  static const double fontLetterSpacing1150 = -0.65;
+  static const double fontLetterSpacing1250 = -0.7;
+  static const double fontLetterSpacing1450 = -0.96;
+  static const double fontLetterSpacing1850 = -1.08;
+
+  static const int fontLineHeight250 = 16;
+  static const int fontLineHeight350 = 20;
+  static const int fontLineHeight450 = 24;
+  static const int fontLineHeight550 = 28;
+  static const int fontLineHeight650 = 32;
+  static const int fontLineHeight750 = 36;
+  static const int fontLineHeight850 = 40;
+  static const int fontLineHeight950 = 44;
+  static const int fontLineHeight1050 = 48;
+  static const int fontLineHeight1150 = 52;
+  static const int fontLineHeight1250 = 56;
+  static const int fontLineHeight1350 = 60;
+  static const int fontLineHeight1450 = 64;
+  static const int fontLineHeight1850 = 72;
+  static const int fontLineHeight2050 = 80;
+
+  static const int fontSize150 = 12;
+  static const int fontSize175 = 13;
+  static const int fontSize200 = 14;
+  static const int fontSize250 = 16;
+  static const int fontSize300 = 18;
+  static const int fontSize350 = 20;
+  static const int fontSize450 = 24;
+  static const int fontSize550 = 28;
+  static const int fontSize650 = 32;
+  static const int fontSize750 = 36;
+  static const int fontSize850 = 40;
+  static const int fontSize950 = 44;
+  static const int fontSize1050 = 48;
+  static const int fontSize1150 = 52;
+  static const int fontSize1250 = 56;
+  static const int fontSize1450 = 64;
+  static const int fontSize1850 = 72;
+
+  static const int fontWeight100 = 100;
+  static const int fontWeight200 = 200;
+  static const int fontWeight300 = 300;
+  static const int fontWeight400 = 400;
+  static const int fontWeight500 = 500;
+  static const int fontWeight600 = 600;
+  static const int fontWeight700 = 700;
+  static const int fontWeight800 = 800;
+  static const int fontWeight900 = 900;
+  static const int fontWeight950 = 950;
+}
