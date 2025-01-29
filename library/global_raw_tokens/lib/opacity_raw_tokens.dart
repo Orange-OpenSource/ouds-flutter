8751a488e741085c7f316cccc425aaeaf761a56d
Tayeb Sedraia <tayeb.sedraia@orange.com>
2025-01-29 10:57:03 +0100

    chore: update global raw tokens for opacity

---
+++ library/global_raw_tokens/lib/opacity_raw_tokens.dart
@@ -0,0 +1,39 @@
+/*
+ * Software Name : OUDS Flutter
+ * SPDX-FileCopyrightText: Copyright (c) Orange SA
+ * SPDX-License-Identifier: MIT
+ *
+ * This software is distributed under the MIT license,
+ * the text of which is available at https://opensource.org/license/MIT/
+ * or see the "LICENSE" file for more details.
+ *
+ * Software description: Flutter library of reusable graphical components for Android and iOS
+ */
+
+class OpacityRawTokens {
+  static const double opacity0 = 0.0;
+  static const double opacity40 = 0.04;
+  static const double opacity80 = 0.08;
+  static const double opacity120 = 0.12;
+  static const double opacity160 = 0.16;
+  static const double opacity200 = 0.2;
+  static const double opacity240 = 0.24;
+  static const double opacity280 = 0.28;
+  static const double opacity320 = 0.32;
+  static const double opacity360 = 0.36;
+  static const double opacity400 = 0.4;
+  static const double opacity440 = 0.44;
+  static const double opacity480 = 0.48;
+  static const double opacity520 = 0.52;
+  static const double opacity560 = 0.56;
+  static const double opacity600 = 0.6;
+  static const double opacity640 = 0.64;
+  static const double opacity680 = 0.68;
+  static const double opacity720 = 0.72;
+  static const double opacity760 = 0.76;
+  static const double opacity800 = 0.8;
+  static const double opacity880 = 0.88;
+  static const double opacity920 = 0.92;
+  static const double opacity960 = 0.96;
+  static const double opacity1000 = 1.0;
+}
