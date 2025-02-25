b5b50e068cd117509e3db285220c573f604ece9f
Tayeb Sedraia <tayeb.sedraia@orange.com>
2025-02-25 17:03:01 +0100

    refactor: update oudsbutton component

--- ouds_core/lib/components/button/ouds_button.dart
+++ ouds_core/lib/components/button/ouds_button.dart
@@ -13,36 +13,59 @@
 import 'package:flutter/material.dart';
 import 'package:ouds_core/ouds_theme.dart';
 
+/// [OUDS Button design guidelines](https://unified-design-system.orange.com/472794e18/p/48a788-button)
+///
+/// An OUDS button which displays text only.
+///
+/// In the case it is used in an [OudsColoredBox], its monochrome variant is automatically displayed.
+/// Some tokens associated with these specific colors can be customized and are identified with the `Mono` suffix (for instance [OudsButtonTokens.colorBgDefaultEnabledMono]).
+///
+/// **Parameters**:
+/// - `text`: Text displayed in the button.
+/// - `onClick`: Callback invoked when the button is clicked.
+/// - `enabled`: Controls the enabled state of the button when [style] is equal to [OudsButton.Style.Default].
+///   When `false`, this button will not be clickable. Has no effect when [style] is equal to [OudsButton.Style.Loading].
+/// - `style`: The button style.
+/// - `hierarchy`: The button hierarchy.
+///   A button with [OudsButton.Hierarchy.Negative] hierarchy is not allowed as a direct or indirect child of an [OudsColoredBox] and will throw an [IllegalStateException].
+///
+/// **Samples**:
+/// - [OudsButtonWithTextSample](com.orange.ouds.core.component.samples.OudsButtonWithTextSample)
+/// - [OudsButtonWithTextOnColoredBackgroundSample](com.orange.ouds.core.component.samples.OudsButtonWithTextOnColoredBackgroundSample)
+
 class OudsButton extends StatelessWidget {
+  /// The button's title displayed inside the button.
   final String label;
-  final VoidCallback onPressed;
+
+  /// The action to be executed when the button is pressed.
+  final Function()? onClick;
 
   const OudsButton({
     super.key,
     required this.label,
-    required this.onPressed,
+    this.onClick,
   });
 
   @override
   Widget build(BuildContext context) {
     final theme = OudsTheme.of(context);
 
-    /// ElevatedButtonTheme
+    /// OutlinedButtonTheme
     return OutlinedButton(
       style: OutlinedButton.styleFrom(
         backgroundColor: theme.componentsTokens.button.colorBgDefaultEnabled,
         textStyle: const TextStyle(fontSize: 19, fontWeight: FontWeight.w500),
+        side: BorderSide(
+            color: theme.componentsTokens.button.colorBorderDefaultEnabled,
+            width: theme.componentsTokens.button.borderWidthDefault),
         shape: RoundedRectangleBorder(
           borderRadius:
               BorderRadius.circular(theme.componentsTokens.button.borderRadius),
-          side: BorderSide(
-              color: theme.componentsTokens.button.colorBorderDefaultEnabled,
-              width: theme.componentsTokens.button.borderWidthDefault),
         ),
         minimumSize: Size(theme.componentsTokens.button.sizeMinWidth,
             theme.componentsTokens.button.sizeMinHeight),
       ),
-      onPressed: onPressed,
+      onPressed: onClick,
       child: Text(label),
     );
   }
