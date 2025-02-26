311a9305dfbc0b6c52af5a90f1ebca4bfd311009
Tayeb Sedraia <tayeb.sedraia@orange.com>
2025-02-26 11:10:41 +0100

    refactor: add button layout and enabled state

--- ouds_core/lib/components/button/ouds_button.dart
+++ ouds_core/lib/components/button/ouds_button.dart
@@ -35,14 +35,18 @@ import 'package:ouds_core/ouds_theme.dart';
 
 class OudsButton extends StatelessWidget {
   /// The button's title displayed inside the button.
-  final String label;
+  final String? label;
 
   /// The action to be executed when the button is pressed.
   final Function()? onClick;
 
+  /// The optional button's icon.
+  final Widget? icon;
+
   const OudsButton({
     super.key,
-    required this.label,
+    this.label,
+    this.icon,
     this.onClick,
   });
 
@@ -50,23 +54,90 @@ class OudsButton extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = OudsTheme.of(context);
 
-    /// OutlinedButtonTheme
+    /// Icon only
+    if (icon != null && label == null) {
+      return IconButton(
+        style: OutlinedButton.styleFrom(
+          backgroundColor: theme.componentsTokens.button.colorBgDefaultEnabled,
+          textStyle: TextStyle(
+              fontSize: theme.fontTokens.sizeLabelLarge, fontWeight: FontWeight.w700, letterSpacing: theme.fontTokens.letterSpacingLabelLarge),
+          side: BorderSide(
+            color:
+                onClick != null ? theme.componentsTokens.button.colorBorderDefaultEnabled : theme.componentsTokens.button.colorBorderDefaultDisabled,
+            width: theme.componentsTokens.button.borderWidthDefault,
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(theme.componentsTokens.button.borderRadius),
+          ),
+          padding: EdgeInsets.all(theme.componentsTokens.button.spaceInsetIconOnly),
+          minimumSize: Size(theme.componentsTokens.button.sizeMinWidth, theme.componentsTokens.button.sizeMinHeight),
+        ),
+        visualDensity: VisualDensity.standard,
+        icon: icon!,
+        onPressed: onClick,
+      );
+    }
+
+    /// Icon + Text
+    if (icon != null && label != null) {
+      return OutlinedButton.icon(
+        style: OutlinedButton.styleFrom(
+          backgroundColor: theme.componentsTokens.button.colorBgDefaultEnabled,
+          textStyle: TextStyle(
+              fontSize: theme.fontTokens.sizeLabelLarge, fontWeight: FontWeight.w700, letterSpacing: theme.fontTokens.letterSpacingLabelLarge),
+          side: BorderSide(
+            color:
+                onClick != null ? theme.componentsTokens.button.colorBorderDefaultEnabled : theme.componentsTokens.button.colorBorderDefaultDisabled,
+            width: theme.componentsTokens.button.borderWidthDefault,
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(theme.componentsTokens.button.borderRadius),
+          ),
+          padding: EdgeInsets.only(
+            top: theme.componentsTokens.button.spacePaddingBlock,
+            right: theme.componentsTokens.button.spacePaddingInlineEndIconStart,
+            bottom: theme.componentsTokens.button.spacePaddingBlock,
+            left: theme.componentsTokens.button.spacePaddingInlineIconStart,
+          ),
+          minimumSize: Size(theme.componentsTokens.button.sizeMinWidth, theme.componentsTokens.button.sizeMinHeight),
+        ),
+        onPressed: onClick,
+        icon: icon,
+        iconAlignment: IconAlignment.start,
+        label: Text(
+          label!,
+          style: TextStyle(
+            color: onClick != null
+                ? theme.componentsTokens.button.colorContentDefaultEnabled
+                : theme.componentsTokens.button.colorContentDefaultDisabled,
+          ),
+        ),
+      );
+    }
+
+    /// Text Only
     return OutlinedButton(
       style: OutlinedButton.styleFrom(
         backgroundColor: theme.componentsTokens.button.colorBgDefaultEnabled,
-        textStyle: const TextStyle(fontSize: 19, fontWeight: FontWeight.w500),
+        textStyle: TextStyle(
+            fontSize: theme.fontTokens.sizeLabelLarge, fontWeight: FontWeight.w700, letterSpacing: theme.fontTokens.letterSpacingLabelLarge),
         side: BorderSide(
-            color: theme.componentsTokens.button.colorBorderDefaultEnabled,
-            width: theme.componentsTokens.button.borderWidthDefault),
-        shape: RoundedRectangleBorder(
-          borderRadius:
-              BorderRadius.circular(theme.componentsTokens.button.borderRadius),
+          color: onClick != null ? theme.componentsTokens.button.colorBorderDefaultEnabled : theme.componentsTokens.button.colorBorderDefaultDisabled,
+          width: theme.componentsTokens.button.borderWidthDefault,
         ),
-        minimumSize: Size(theme.componentsTokens.button.sizeMinWidth,
-            theme.componentsTokens.button.sizeMinHeight),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(theme.componentsTokens.button.borderRadius),
+        ),
+        padding: EdgeInsets.only(
+          top: theme.componentsTokens.button.spacePaddingBlock,
+          right: theme.componentsTokens.button.spacePaddingInlineIconNone,
+          bottom: theme.componentsTokens.button.spacePaddingBlock,
+          left: theme.componentsTokens.button.spacePaddingInlineIconNone,
+        ),
+        minimumSize: Size(theme.componentsTokens.button.sizeMinWidth, theme.componentsTokens.button.sizeMinHeight),
       ),
       onPressed: onClick,
-      child: Text(label),
+      child: Text(label!),
     );
   }
 }
