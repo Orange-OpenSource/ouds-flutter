0f7a452fd735558995b4d951b942ec93b8283909
Ahmed Amine Zribi <ahmedamine.zribi@sofrecom>
2025-04-16 17:07:11 +0100

    feat: Add ouds radio button file

---
+++ ouds_core/lib/components/radio/ouds_radio_button.dart
@@ -0,0 +1,27 @@
+/*
+ * // Software Name: OUDS Flutter
+ * // SPDX-FileCopyrightText: Copyright (c) Orange SA
+ * // SPDX-License-Identifier: MIT
+ * //
+ * // This software is distributed under the MIT license,
+ * // the text of which is available at https://opensource.org/license/MIT/
+ * // or see the "LICENSE" file for more details.
+ * //
+ * // Software description: Flutter library of reusable graphical components
+ * //
+ */
+
+// class OudsRadioButton extends StatefulWidget {
+//   final bool value;
+//   final ValueChanged<bool>? onChanged;
+//   final bool enabled;
+//   final bool error;
+//
+//   const OudsRadioButton({
+//     super.key,
+//     required this.value,
+//     this.onChanged,
+//     this.enabled = true,
+//     this.error = false,
+//   });
+// }
