103a74e624c29444717b9e63a766da941ff580f4
n.hammami <nouha.hammami@sofrecom.com>
2025-04-16 15:32:15 +0100

    feat: add arabic translations for Demo App (Google Translated) #134

---
+++ app/lib/l10n/gen/ouds_flutter_app_localizations_ar.dart
@@ -0,0 +1,137 @@
+import 'ouds_flutter_app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get app_name => 'OudsPlayground';
+
+  @override
+  String get app_common_selected_a11y => 'محدد';
+
+  @override
+  String get app_common_unselected_a11y => 'غير محدد';
+
+  @override
+  String get app_common_back_a11y => 'رجوع';
+
+  @override
+  String get app_common_copyCode_a11y => 'نسخ الرمز';
+
+  @override
+  String get app_common_codeCopied_text => 'تم نسخ الرمز إلى الحافظة';
+
+  @override
+  String get app_common_collapsed_a11y => 'مطوي';
+
+  @override
+  String get app_common_expanded_a11y => 'موسّع';
+
+  @override
+  String get app_common_bottomSheetExpanded_a11y => 'تم توسيع اللوحة السفلية';
+
+  @override
+  String get app_common_bottomSheetCollapsed_a11y => 'تم طي اللوحة السفلية';
+
+  @override
+  String get app_common_customize_label => 'تخصيص';
+
+  @override
+  String get app_common_enabled_label => 'مُفعّل';
+
+  @override
+  String get app_common_onColoredBackground_label => 'على خلفية ملوّنة';
+
+  @override
+  String get app_common_customizeChips_label_a11y => 'قائمة الشرائح';
+
+  @override
+  String get app_common_customizeChips_hint_a11y => 'يرجى اختيار شريحة من خلال التنقل بين الخيارات';
+
+  @override
+  String get app_topBar_theme_button_a11y => 'وضع السمة';
+
+  @override
+  String get app_topBar_theme_hint_a11y => 'اضغط لتغيير وضع السمة';
+
+  @override
+  String get app_topBar_darkMode_button_a11y => 'داكن';
+
+  @override
+  String get app_topBar_lightMode_button_a11y => 'فاتح';
+
+  @override
+  String get app_topBar_systemMode_button_a11y => 'النظام';
+
+  @override
+  String get app_bottomBar_tokens_label => 'الرموز';
+
+  @override
+  String get app_bottomBar_components_label => 'المكوّنات';
+
+  @override
+  String get app_bottomBar_about_label => 'حول';
+
+  @override
+  String get app_tokens_viewCodeExample_label => 'عرض مثال رمز التوكن';
+
+  @override
+  String get app_components_common_layout_label => 'التخطيط';
+
+  @override
+  String get app_components_common_textOnlyLayout_label => 'نص فقط';
+
+  @override
+  String get app_components_common_iconAndTextLayout_label => 'أيقونة + نص';
+
+  @override
+  String get app_components_common_style_label => 'النمط';
+
+  @override
+  String get app_components_common_text_label => 'نص';
+
+  @override
+  String get app_components_common_onColoredBackground_label => 'على خلفية ملوّنة';
+
+  @override
+  String get app_components_button_label => 'زر';
+
+  @override
+  String get app_components_button_description_text => 'تتيح الأزرار للمستخدمين اتخاذ قرارات أو تنفيذ إجراء. لها أنماط متعددة لتناسب مختلف الاحتياجات.';
+
+  @override
+  String get app_components_button_hierarchy_label => 'التسلسل';
+
+  @override
+  String get app_components_button_iconOnlyLayout_label => 'أيقونة فقط';
+
+  @override
+  String get app_components_button_icon_a11y => 'أيقونة';
+
+  @override
+  String get app_tokens_elevation_label => 'الارتفاع';
+
+  @override
+  String get app_tokens_elevation_description_text => 'تُستخدم الظلال لإعطاء انطباع بالمسافة أو الارتفاع بين الأسطح، مما يضيف عمقًا لتصميماتنا.';
+
+  @override
+  String get app_about_name_label => 'أداة نظام التصميم';
+
+  @override
+  String get app_about_privacyPolicy_label => 'سياسة الخصوصية';
+
+  @override
+  String get app_about_legalInformation_label => 'المعلومات القانونية';
+
+  @override
+  String get app_about_materialComponents_label => 'مكوّنات Material 3';
+
+  @override
+  String get app_about_changelog_label => 'سجل التغييرات';
+
+  @override
+  String get app_about_appSettings_label => 'افتح إعدادات التطبيق';
+}
