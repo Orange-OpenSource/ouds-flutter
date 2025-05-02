0f9bf4029fee58cf41ee728abe75d3c3a53dd753
Tayeb Sedraia <tayeb.sedraia@orange.com>
2025-05-02 11:56:27 +0200

    fix: assets svg and add file in NOTICE.txt

--- ouds_core/lib/components/checkbox/ouds_checkbox.dart
+++ ouds_core/lib/components/checkbox/ouds_checkbox.dart
@@ -60,13 +60,8 @@ class OudsCheckbox extends StatefulWidget {
 }
 
 class _OudsCheckboxState extends State<OudsCheckbox> {
-  String svgChecked = '''<svg width="12" height="10" viewBox="0 0 12 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path fill-rule="evenodd" clip-rule="evenodd" d="M11.2621 2.04661L5.22765 9.04663C5.05487 9.24679 4.78924 9.375 4.49138 9.375C4.1935 9.375 3.92806 9.24668 3.75525 9.04653L0.737872 5.54661C0.6086 5.39683 0.53125 5.20683 0.53125 5C0.53125 4.75837 0.636777 4.53964 0.807402 4.38129L1.18458 4.03129C1.3552 3.87293 1.59092 3.775 1.85129 3.775C2.08562 3.775 2.29985 3.85452 2.46476 3.98583L4.30279 5.87502L9.88099 0.861666C10.0496 0.714884 10.2764 0.625 10.5258 0.625C11.0466 0.625 11.4688 1.01676 11.4688 1.50002C11.4688 1.70681 11.3914 1.89683 11.2621 2.04661Z" fill="#F15E00"/>
-</svg>''';
-
-  String svgIndeterminate = '''<svg width="12" height="2" viewBox="0 0 12 2" fill="none" xmlns="http://www.w3.org/2000/svg">
-<rect width="12" height="2" fill="#F15E00"/>
-</svg>''';
+  // The name of the package where the asset is located
+  String packageName = 'ouds_core';
 
   bool _isHovered = false;
   bool _isPressed = false;
@@ -152,8 +147,9 @@ class _OudsCheckboxState extends State<OudsCheckbox> {
                         ? Align(
                             alignment: Alignment.center,
                             child: SizedBox(
-                              child: SvgPicture.string(
-                                svgChecked,
+                              child: SvgPicture.asset(
+                                'assets/checkbox_selected.svg',
+                                package: packageName,
                                 fit: BoxFit.contain,
                                 colorFilter: ColorFilter.mode(
                                   checkboxTickModifier.getTickColor(checkboxState, widget.isError),
@@ -166,8 +162,9 @@ class _OudsCheckboxState extends State<OudsCheckbox> {
                             ? null
                             : Align(
                                 alignment: Alignment.center,
-                                child: SvgPicture.string(
-                                  svgIndeterminate,
+                                child: SvgPicture.asset(
+                                  'assets/checkbox_indeterminate.svg',
+                                  package: packageName,
                                   fit: BoxFit.contain,
                                   colorFilter: ColorFilter.mode(
                                     checkboxTickModifier.getTickColor(checkboxState, widget.isError),
