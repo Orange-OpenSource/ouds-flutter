bb970e36c51265cfb96e1c05f24f3a5b540f6353
Tayeb Sedraia <tayeb.sedraia@orange.com>
2025-05-06 11:34:36 +0200

    review : vertical max height item and icon

--- ouds_core/lib/components/control/ouds_control_item.dart
+++ ouds_core/lib/components/control/ouds_control_item.dart
@@ -77,87 +77,82 @@ class OudsControlItemState extends State<OudsControlItem> {
     final controlItemBackgroundModifier = OudsControlBackgroundModifier(context);
 
     return Padding(
-      // Apply symmetric horizontal padding based on the token value
       padding: EdgeInsetsDirectional.symmetric(
         horizontal: OudsTheme.of(context).componentsTokens.controlItem.spaceInset,
       ),
-      child: Container(
-        // Container with decoration and constraints
-        decoration: BoxDecoration(
-          // Set background color based on the control item's state
-          color: controlItemBackgroundModifier.getBackgroundColor(controlItemState),
-          // Transparent border with zero width
-          border: Border.all(
-            color: Colors.transparent,
-            width: 0.0,
-          ),
-          // Rounded corners based on the token border radius
-          borderRadius: BorderRadius.circular(OudsTheme.of(context).borderTokens.radiusNone),
-        ),
-        constraints: BoxConstraints(
-          // Minimum height and width defined by the token component tokens
-          minHeight: OudsTheme.of(context).componentsTokens.controlItem.sizeMinHeight,
-          minWidth: OudsTheme.of(context).componentsTokens.controlItem.sizeMinWidth,
-        ),
-        child: Column(
-          children: [
-            Container(
-              // Ensure minimum height for the inner container
-              constraints: BoxConstraints(
-                minHeight: OudsTheme.of(context).componentsTokens.controlItem.sizeMinHeight,
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              // Set background color based on the control item's state
+              color: controlItemBackgroundModifier.getBackgroundColor(controlItemState),
+              // Transparent border with zero width
+              border: Border.all(
+                color: Colors.transparent,
+                width: 0.0,
               ),
-              child: InkWell(
-                // Handle tap events if not in read-only mode
-                onTap: !widget.readOnly ? widget.onTap : null,
-                // Update pressed state on highlight change
-                onHighlightChanged: (isPressed) {
-                  setState(() {
-                    _isPressed = isPressed;
-                    interactionController.setPressed(isPressed);
-                  });
-                },
-                // Update hovered state on hover change
-                onHover: (hovering) {
-                  setState(() {
-                    _isHovered = hovering;
-                    interactionController.setHovered(hovering);
-                  });
-                },
-                // Disable default highlight and splash animations
-                highlightColor: Colors.transparent,
-                hoverColor: OudsTheme.of(context).componentsTokens.controlItem.colorBgHover,
-                splashColor: Colors.transparent,
-                child: Padding(
-                  // Inner padding based on the token inset value
-                  padding: EdgeInsetsDirectional.all(
-                    OudsTheme.of(context).componentsTokens.controlItem.spaceInset,
-                  ),
+              borderRadius: BorderRadius.circular(OudsTheme.of(context).borderTokens.radiusNone),
+            ),
+            // Ensure minimum height for the item container
+            constraints: BoxConstraints(
+              minHeight: OudsTheme.of(context).componentsTokens.controlItem.sizeMinHeight,
+              minWidth: OudsTheme.of(context).componentsTokens.controlItem.sizeMinWidth,
+            ),
+            child: InkWell(
+              // Handle tap events if not in read-only mode
+              onTap: !widget.readOnly ? widget.onTap : null,
+              onHighlightChanged: (isPressed) {
+                setState(() {
+                  _isPressed = isPressed;
+                  interactionController.setPressed(isPressed);
+                });
+              },
+              onHover: (hovering) {
+                setState(() {
+                  _isHovered = hovering;
+                  interactionController.setHovered(hovering);
+                });
+              },
+              highlightColor: Colors.transparent,
+              hoverColor: OudsTheme.of(context).componentsTokens.controlItem.colorBgHover,
+              splashColor: Colors.transparent,
+              child: Padding(
+                padding: EdgeInsetsDirectional.all(
+                  OudsTheme.of(context).componentsTokens.controlItem.spaceInset,
+                ),
+                child: IntrinsicHeight(
                   child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    // Build layout based on the reversed flag
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: !widget.reversed ? _buildStandardLayout(controlItemState) : _buildInvertedLayout(controlItemState),
                   ),
                 ),
               ),
             ),
-            // Optional divider below the control item if specified
-            if (widget.divider)
-              Divider(
-                height: 0,
-                thickness: OudsTheme.of(context).borderTokens.widthDefault,
-              ),
-          ],
-        ),
+          ),
+          // Optional divider below the control item if specified
+          if (widget.divider)
+            Divider(
+              height: 0,
+              thickness: OudsTheme.of(context).borderTokens.widthDefault,
+            ),
+        ],
       ),
     );
   }
 
   List<Widget> _buildStandardLayout(OudsControlState controlItemState) => [
         AbsorbPointer(
-          child: SizedBox(
-            height: OudsTheme.of(context).componentsTokens.controlItem.sizeLoader,
-            width: OudsTheme.of(context).componentsTokens.controlItem.sizeLoader,
-            child: widget.indicator(),
+          child: Container(
+            constraints: BoxConstraints(
+              maxHeight: OudsTheme.of(context).componentsTokens.controlItem.sizeMaxHeightAssetsContainer,
+              minHeight: OudsTheme.of(context).componentsTokens.controlItem.sizeIcon,
+            ),
+            alignment: Alignment.center,
+            child: SizedBox(
+              height: OudsTheme.of(context).componentsTokens.checkbox.sizeIndicator,
+              width: OudsTheme.of(context).componentsTokens.checkbox.sizeIndicator,
+              child: widget.indicator(),
+            ),
           ),
         ),
         Container(
@@ -169,38 +164,57 @@ class OudsControlItemState extends State<OudsControlItem> {
             width: OudsTheme.of(context).componentsTokens.controlItem.spaceColumnGap,
           ),
         if (widget.icon != null)
-          SizedBox(
-            height: OudsTheme.of(context).componentsTokens.controlItem.sizeIcon,
-            width: OudsTheme.of(context).componentsTokens.controlItem.sizeIcon,
-            child: OudsControlItem.buildIcon(
-              context,
-              widget.icon!,
-              controlItemState,
-              false,
+          Container(
+            constraints: BoxConstraints(
+              maxHeight: OudsTheme.of(context).componentsTokens.controlItem.sizeMaxHeightAssetsContainer,
+            ),
+            alignment: Alignment.center,
+            child: SizedBox(
+              height: OudsTheme.of(context).componentsTokens.controlItem.sizeIcon,
+              width: OudsTheme.of(context).componentsTokens.controlItem.sizeIcon,
+              child: OudsControlItem.buildIcon(
+                context,
+                widget.icon!,
+                controlItemState,
+                false,
+              ),
             ),
           ),
       ];
 
   List<Widget> _buildInvertedLayout(OudsControlState controlItemState) => [
         if (widget.icon != null)
-          SizedBox(
-            height: OudsTheme.of(context).componentsTokens.controlItem.sizeIcon,
-            width: OudsTheme.of(context).componentsTokens.controlItem.sizeIcon,
-            child: OudsControlItem.buildIcon(
-              context,
-              widget.icon!,
-              controlItemState,
-              false,
+          Container(
+            constraints: BoxConstraints(
+              maxHeight: OudsTheme.of(context).componentsTokens.controlItem.sizeMaxHeightAssetsContainer,
+            ),
+            alignment: Alignment.center,
+            child: SizedBox(
+              height: OudsTheme.of(context).componentsTokens.controlItem.sizeIcon,
+              width: OudsTheme.of(context).componentsTokens.controlItem.sizeIcon,
+              child: OudsControlItem.buildIcon(
+                context,
+                widget.icon!,
+                controlItemState,
+                false,
+              ),
             ),
           ),
         if (widget.icon != null) SizedBox(width: OudsTheme.of(context).componentsTokens.controlItem.spaceColumnGap),
         _buildTextWithAdditionalAndHelper(controlItemState),
         SizedBox(width: OudsTheme.of(context).componentsTokens.controlItem.spaceColumnGap),
         AbsorbPointer(
-          child: SizedBox(
-            height: OudsTheme.of(context).componentsTokens.controlItem.sizeLoader,
-            width: OudsTheme.of(context).componentsTokens.controlItem.sizeLoader,
-            child: widget.indicator(),
+          child: Container(
+            constraints: BoxConstraints(
+              maxHeight: OudsTheme.of(context).componentsTokens.controlItem.sizeMaxHeightAssetsContainer,
+              minHeight: OudsTheme.of(context).componentsTokens.controlItem.sizeIcon,
+            ),
+            alignment: Alignment.center,
+            child: SizedBox(
+              height: OudsTheme.of(context).componentsTokens.checkbox.sizeIndicator,
+              width: OudsTheme.of(context).componentsTokens.checkbox.sizeIndicator,
+              child: widget.indicator(),
+            ),
           ),
         ),
       ];
