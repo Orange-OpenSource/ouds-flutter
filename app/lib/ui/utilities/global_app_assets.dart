fd455147b875994033db881eecdb64b27fe04870
Ahmed Amine Zribi <ahmedamine.zribi@sofrecom>
2025-05-09 15:42:48 +0100

    feat: First commit refactor icons Apps

---
+++ app/lib/ui/utilities/global_app_assets.dart
@@ -0,0 +1,53 @@
+/*
+ * // Software Name: OUDS Flutter
+ * // SPDX-FileCopyrightText: Copyright (c) Orange SA
+ * // SPDX-License-Identifier: MIT
+ * //
+ * // This software is distributed under the MIT license,
+ * // the text of which is available at https://opensource.org/license/MIT/
+ * // or see the "LICENSE" file for more details.
+ * //
+ * // Software description: Flutter library of reusable graphical components
+ * //
+ */
+
+class AppAssets {
+  static const images = _Images();
+  static const icons = _Icons();
+  static const fonts = _Fonts();
+  static const animations = _Animations();
+}
+
+class _Images {
+  const _Images();
+  /// Components
+  final ilComponentsButton = 'assets/il_components_button.svg';
+  final ilComponentsButtonDark = 'assets/il_components_button_dark.svg';
+
+  ///Tokens
+  final ilTokensColor = 'assets/il_tokens_color.svg';
+  final ilTokensColorDark = 'assets/il_tokens_color_dark.svg';
+
+  final ilTokensElevation = 'assets/il_tokens_elevation.svg';
+  final ilTokensElevationDark = 'assets/il_tokens_elevation_dark.svg';
+
+  final ilTokensOpacity = 'assets/il_tokens_opacity.svg';
+  final ilTokensOpacityDark = 'assets/il_tokens_opacity_dark.svg';
+
+  final ilUnion = 'assets/il_union.png';
+  final ilUnionDark = 'assets/il_union_dark.png';
+}
+
+class _Icons {
+  const _Icons();
+  final icToken = 'assets/ic_token.svg';
+  final home = 'assets/ic_theme_system.svg';
+}
+
+class _Fonts {
+  const _Fonts();
+}
+
+class _Animations {
+  const _Animations();
+}
