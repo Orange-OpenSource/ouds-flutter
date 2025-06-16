ca23c400e06434b5baaad03f645a388a23ba5c5a
Tayeb Sedraia <tayeb.sedraia@orange.com>
2025-06-16 18:30:58 +0200

    fix : a11y vocalisation
    Signed-off-by: Tayeb Sedraia <tayeb.sedraia@orange.com>

--- app/lib/ui/utilities/customizable/customizable_dropdown_menu.dart
+++ app/lib/ui/utilities/customizable/customizable_dropdown_menu.dart
@@ -10,6 +10,7 @@
 // Software description: Flutter library of reusable graphical components
 //
 import 'package:flutter/material.dart';
+import 'package:ouds_flutter_demo/l10n/app_localizations.dart';
 import 'package:ouds_flutter_demo/ui/theme/theme_controller.dart';
 import 'package:ouds_theme_contract/ouds_theme.dart';
 import 'package:provider/provider.dart';
@@ -44,39 +45,54 @@ class CustomizationDropdownMenu<T> extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Padding(
-          padding: EdgeInsetsDirectional.symmetric(horizontal: currentTheme.spaceScheme(context).fixedMedium),
-          child: Text(
-            label,
-            style: currentTheme.typographyTokens.typeBodyStrongLarge(context),
+        ExcludeSemantics(
+          child: Padding(
+            padding: EdgeInsetsDirectional.symmetric(horizontal: currentTheme.spaceScheme(context).fixedMedium),
+            child: Text(
+              label,
+              style: currentTheme.typographyTokens.typeBodyStrongLarge(context),
+            ),
           ),
         ),
         Padding(
           padding: EdgeInsetsDirectional.symmetric(
-              horizontal: currentTheme.spaceScheme(context).fixedMedium, vertical: currentTheme.spaceScheme(context).fixedShorter),
-          child: DropdownMenu<T>(
-            initialSelection: options[selectedItemIndex],
-            expandedInsets: EdgeInsets.zero,
-            inputDecorationTheme: const InputDecorationTheme(isDense: true),
-            textStyle: currentTheme.typographyTokens.typeBodyStrongLarge(context),
-            onSelected: (value) {
-              if (value != null) {
-                final newIndex = options.indexOf(value);
-                onSelectionChange(value, newIndex);
-              }
-            },
-            leadingIcon: itemLeadingIcons != null ? buildDropdownLeadingIcon(context, itemLeadingIcons, selectedItemIndex) : null,
-            dropdownMenuEntries: List.generate(options.length, (index) {
-              return DropdownMenuEntry<T>(
-                labelWidget: Text(
-                  getText(options[index]),
-                  style: currentTheme.typographyTokens.typeBodyStrongLarge(context),
-                ),
-                value: options[index],
-                label: getText(options[index]),
-                leadingIcon: itemLeadingIcons != null ? buildDropdownLeadingIcon(context, itemLeadingIcons, index) : null,
-              );
-            }),
+            horizontal: currentTheme.spaceScheme(context).fixedMedium,
+            vertical: currentTheme.spaceScheme(context).fixedShorter,
+          ),
+          child: Semantics(
+            selected: options.contains(options[selectedItemIndex]),
+            child: DropdownMenu<T>(
+              initialSelection: options[selectedItemIndex],
+              expandedInsets: EdgeInsets.zero,
+              inputDecorationTheme: const InputDecorationTheme(isDense: true),
+              textStyle: currentTheme.typographyTokens.typeBodyStrongLarge(context),
+              onSelected: (value) {
+                if (value != null) {
+                  final newIndex = options.indexOf(value);
+                  onSelectionChange(value, newIndex);
+                }
+              },
+              leadingIcon: itemLeadingIcons != null ? buildDropdownLeadingIcon(context, itemLeadingIcons, selectedItemIndex) : null,
+              dropdownMenuEntries: List.generate(
+                options.length,
+                (index) {
+                  final isSelected = options[index] == options[selectedItemIndex];
+                  return DropdownMenuEntry<T>(
+                    labelWidget: Semantics(
+                      button: true,
+                      value: isSelected ? context.l10n.app_common_selected_a11y : context.l10n.app_common_unselected_a11y,
+                      child: Text(
+                        getText(options[index]),
+                        style: currentTheme.typographyTokens.typeBodyStrongLarge(context),
+                      ),
+                    ),
+                    value: options[index],
+                    label: getText(options[index]),
+                    leadingIcon: itemLeadingIcons != null ? buildDropdownLeadingIcon(context, itemLeadingIcons, index) : null,
+                  );
+                },
+              ),
+            ),
           ),
         ),
       ],
