ad81996cbdbacfb8a6fcb4eae805851e62d1b83e
Ahmed Amine Zribi <ahmedamine.zribi@sofrecom>
2025-06-27 17:53:23 +0100

    feat: Add first Variant Badge

--- app/lib/l10n/gen/ouds_flutter_app_localizations_ar.dart
+++ app/lib/l10n/gen/ouds_flutter_app_localizations_ar.dart
@@ -249,6 +249,13 @@ class AppLocalizationsAr extends AppLocalizations {
       'نص إضافي';
 
   @override
+  String get app_components_badge_label => 'Badge';
+
+  @override
+  String get app_components_badge_description_text =>
+      'The Badge is a small UI element used to highlight status, notifications, or categorization within an interface. It is often displayed as a label or indicator with a distinct background color and text.';
+
+  @override
   String get app_about_name_label => 'أداة نظام التصميم';
 
   @override
---
+++ app/lib/ui/components/badge/badge_customization.dart
@@ -0,0 +1,146 @@
+/*
+ * // Software Name: OUDS Flutter
+ * // SPDX-FileCopyrightText: Copyright (c) Orange SA
+ * // SPDX-License-Identifier: MIT
+ * //
+ * // This software is distributed under the MIT license,
+ * // the text of which is available at https://opensource.org/license/MIT/
+ * // or see the "LICENSE" file for more details.
+ * //
+ * // Software description: Flutter library of reusable graphical components
+ * //
+ */
+
+import 'package:flutter/material.dart';
+import 'package:ouds_flutter_demo/ui/components/badge/badge_enum.dart';
+import 'package:ouds_flutter_demo/ui/utilities/customizable/customizable_widget_state.dart';
+
+/// Section for InheritedWidget to pass data down the widget tree
+class _BadgeCustomization extends InheritedWidget {
+  const _BadgeCustomization({
+    required super.child,
+    required this.data,
+  });
+
+  final BadgeCustomizationState data;
+
+  @override
+  bool updateShouldNotify(_BadgeCustomization oldWidget) => true;
+}
+
+/// Main Widget class for button customization
+class BadgeCustomization extends StatefulWidget {
+  const BadgeCustomization({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  BadgeCustomizationState createState() => BadgeCustomizationState();
+
+  static BadgeCustomizationState? of(BuildContext context) {
+    return (context.dependOnInheritedWidgetOfExactType<_BadgeCustomization>())?.data;
+  }
+}
+
+class BadgeCustomizationState extends CustomizationWidgetState<BadgeCustomization> {
+  late final SizeState sizeState;
+  late final StatusState statusState;
+
+  @override
+  void initState() {
+    super.initState();
+    sizeState = SizeState(setState);
+    statusState = StatusState(setState);
+  }
+
+  // size State Management
+  BadgeEnumSize get selectedState => sizeState.selected;
+  set selectedState(BadgeEnumSize value) => sizeState.selected = value;
+
+  // Proxy getters and setters to expose state values directly
+  BadgeEnumStatus get selectedStatus => statusState.selectedStatus;
+  set selectedStatus(BadgeEnumStatus value) => statusState.selectedStatus = value;
+
+  int get selectedIndex => statusState.index;
+  set selectedIndex(int value) => statusState.index = value;
+
+  @override
+  Widget build(BuildContext context) {
+    return _BadgeCustomization(
+      data: this,
+      child: widget.child,
+    );
+  }
+}
+
+/// Hierarchy State Management
+class SizeState {
+  SizeState(this._setState);
+
+  final void Function(void Function()) _setState;
+
+  List<BadgeEnumSize> _size = [
+    BadgeEnumSize.xsmall,
+    BadgeEnumSize.small,
+    BadgeEnumSize.medium,
+    BadgeEnumSize.large,
+  ];
+  BadgeEnumSize _selectedSize = BadgeEnumSize.small;
+
+  List<BadgeEnumSize> get list => _size;
+  set list(List<BadgeEnumSize> newList) {
+    _setState(() {
+      _size = newList;
+    });
+  }
+
+  BadgeEnumSize get selected => _selectedSize;
+  set selected(BadgeEnumSize newValue) {
+    _setState(() {
+      _selectedSize = newValue;
+    });
+  }
+}
+
+/// Status State Management
+class StatusState {
+  StatusState(this._setState);
+
+  final void Function(void Function()) _setState;
+
+  List<BadgeEnumStatus> _status = [
+    BadgeEnumStatus.alert,
+    BadgeEnumStatus.positive,
+    BadgeEnumStatus.info,
+    BadgeEnumStatus.warning,
+    BadgeEnumStatus.neutral,
+    BadgeEnumStatus.disabled,
+  ];
+
+  BadgeEnumStatus _selectedStatus = BadgeEnumStatus.alert;
+  int _selectedIndex = 0;
+
+  List<BadgeEnumStatus> get list => _status;
+  set list(List<BadgeEnumStatus> newList) {
+    _setState(() {
+      _status = newList;
+    });
+  }
+
+  BadgeEnumStatus get selectedStatus => _selectedStatus;
+  set selectedStatus(BadgeEnumStatus newValue) {
+    _setState(() {
+      _selectedStatus = newValue;
+    });
+  }
+
+  int get index => _selectedIndex;
+  set index(int newValue) {
+    _setState(() {
+      _selectedIndex = newValue;
+    });
+  }
+}
