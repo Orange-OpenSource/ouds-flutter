b51c606a10b71b5d883fbcc2089f4d3e11c85473
Tayeb Sedraia <tayeb.sedraia@orange.com>
2025-07-28 15:50:35 +0200

    fix: missing radio button token

--- app/lib/ui/utilities/code.dart
+++ app/lib/ui/utilities/code.dart
@@ -146,7 +146,7 @@ class CodeState extends State<Code> with SingleTickerProviderStateMixin {
           horizontal: theme.currentTheme.spaceScheme(context).paddingInlineMedium,
         ),
         decoration: BoxDecoration(
-          color: theme.currentTheme.colorScheme(context).surfaceStatusNeutralMuted,
+          color: theme.currentTheme.colorScheme(context).bgSecondary,
           borderRadius: BorderRadius.circular(theme.currentTheme.borderTokens.radiusDefault),
           border: Border.all(
             color: theme.currentTheme.colorScheme(context).borderDefault,
